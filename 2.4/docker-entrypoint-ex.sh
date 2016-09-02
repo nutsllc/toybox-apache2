23826c2abbe5c915280eb026bcab2ac4c9139cd5
@ontheroad_jp <dev@ontheroad.jp>
2016-09-02 14:17:09 +0900

    [Fix] UID & GID setting

--- 2.4/docker-entrypoint-ex.sh
+++ 2.4/docker-entrypoint-ex.sh
@@ -4,22 +4,24 @@ set -e
 user="www-data"
 group="www-data"
 
-if [ -n "${TOYBOX_GID+x}" ]; then
+if [ -n "${TOYBOX_GID+x}" ] && ! cat /etc/group | grep ":${TOYBOX_GID}:" > /dev/null 2>&1; then
     groupmod -g ${TOYBOX_GID} ${group}
+    echo "GID of ${group} has been changed."
 fi
 
-if [ -n "${TOYBOX_UID+x}" ]; then
+if [ -n "${TOYBOX_UID+x}" ] && ! cat /etc/passwd | grep ":${TOYBOX_UID}:" > /dev/null 2>&1; then
     usermod -u ${TOYBOX_UID} ${user}
+    echo "UID of ${user} has been changed."
 fi
 
 docroot="/usr/local/apache2/htdocs"
 mkdir -p ${docroot}
-tar xvzf /usr/src/apache2-default-doc.tar.gz -C ${docroot}
+tar xzf /usr/src/apache2-default-doc.tar.gz -C ${docroot}
 chown -R ${user}:${group} ${docroot}
 
 confdir="/etc/apache2"
 mkdir -p ${confdir}
-tar xvzf /usr/src/apache2-conf.tar.gz -C ${confdir}
+tar xzf /usr/src/apache2-conf.tar.gz -C ${confdir}
 chown -R ${user}:${group} ${confdir}
 
 exec httpd-foreground
