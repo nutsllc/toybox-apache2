2584b3f6e8dee3b12058ee3d90a1e4caeff0703d
@ontheroad_jp <dev@ontheroad.jp>
2016-09-02 11:42:21 +0900

    [Fix] docroot dir path

--- 2.4/docker-entrypoint-ex.sh
+++ 2.4/docker-entrypoint-ex.sh
@@ -12,7 +12,7 @@ if [ -n "${TOYBOX_UID+x}" ]; then
     usermod -u ${TOYBOX_UID} ${user}
 fi
 
-docroot="/var/www/html"
+docroot="/usr/local/apache2/htdocs"
 mkdir -p ${docroot}
 tar xvzf /usr/src/apache2-default-doc.tar.gz -C ${docroot}
 chown -R ${user}:${group} ${docroot}
